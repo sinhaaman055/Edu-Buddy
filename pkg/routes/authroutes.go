cf7f54249227582b913130d53858335c3c993e1c
sinhaaman055 <sinhaaman055@gmail.com>
2026-06-20 03:49:53 +0530

    Just defining new function for resetpass middleware

--- pkg/routes/authroutes.go
+++ pkg/routes/authroutes.go
@@ -11,6 +11,6 @@ func AuthRoute(r *gin.Engine) {
    r.POST("/signup",handlers.Signup)
    r.POST("/login",handlers.Login)
    r.POST("/forgetpassword",handlers.ForgetPass)
-   r.PATCH("/resetpassword",middleware.Authenticate(),handlers.ResetPass)
+   r.PATCH("/resetpassword",middleware.AuthenticateReserToken(),handlers.ResetPass)
    r.POST("/home/deleteaccount",middleware.Authenticate(),handlers.DeleteAccount)
 }
