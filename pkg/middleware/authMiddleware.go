dcd2fa2c3770b6383da0c588039ea8ceeac64361
sinhaaman055 <sinhaaman055@gmail.com>
2026-06-20 03:40:54 +0530

    implementing another middle ware for resetPass to remove bug due to which token generated by login  not used by resetpass

--- pkg/middleware/authMiddleware.go
+++ pkg/middleware/authMiddleware.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -39,4 +40,36 @@ func Authenticate() gin.HandlerFunc{
 		}
 		c.Next()
 	}
+} 
+func AuthenticateReserToken() gin.HandlerFunc{
+	return func(c *gin.Context){
+		token:=c.Request.Header.Get("Authorization")
+		if token==""{
+			c.JSON(http.StatusBadRequest,gin.H{"Message":"No token Available"})
+			c.Abort()
+			return 
+		}
+		extracted:=strings.Split(token," ")
+		if len(extracted)!=2{
+			c.JSON(400,gin.H{"Message":"Token is not in format"})
+			c.Abort()
+			return
+		}
+		tokenstr:=extracted[1]
+		finalToken,err:=jwt.Parse(tokenstr,func(t *jwt.Token) (any, error) {
+			return []byte(os.Getenv("RESET_KEY")),nil
+		})
+		if err!=nil || !finalToken.Valid{
+			c.JSON(http.StatusBadRequest,gin.H{"Message":"Invalid token"})
+			c.Abort()
+			return
+		}
+		claims,ok:=finalToken.Claims.(jwt.MapClaims)
+		if ok && finalToken.Valid{
+			c.Set("email", claims["email"])
+			c.Set("username", claims["username"])
+		}
+		c.Next()
+	}
+
 }
