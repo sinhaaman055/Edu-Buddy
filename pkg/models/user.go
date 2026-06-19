ab639fe2f05a2b84e60923937f2b482590cc3e41
sinhaaman055 <sinhaaman055@gmail.com>
2026-06-20 03:52:41 +0530

    deleting spaces

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,15 +1,13 @@
 package models
-
 import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type User struct {
-	Name string             `bson:"name" json:"name" binding:"required"`
-	Id  primitive.ObjectID  `bson:"_id,omitempty" json:"id"`
-	Email string            `bson:"email" json:"email" binding:"required" validate:"required,email"`
-	Username string         `bson:"username" json:"username"`
-	Gender string           `bson:"gender" json:"gender" binding:"required"`
-	Password string         `bson:"password" json:"password" binding:"required" validate:"required,min=7"`
-	ConfirmPass string      `bson:"-" json:"confirmPass" validate:"required"`
-	Country string          `bson:"country" json:"country" binding:"required"`
-	DOB string              `bson:"dob" json:"dob" binding:"required"`
+	Name string                     `bson:"name" json:"name" binding:"required"`
+	ID  primitive.ObjectID          `bson:"_id,omitempty" json:"id"`
+	Email string                    `bson:"email" json:"email" binding:"required" validate:"required,email"`
+	Username string                 `bson:"username" json:"username"`
+	Gender string                   `bson:"gender" json:"gender" binding:"required"`
+	Password string                 `bson:"password" json:"password" binding:"required" validate:"required,min=7"`
+	ConfirmPass string              `bson:"-" json:"confirmPass" validate:"required"`
+	Country string                  `bson:"country" json:"country" binding:"required"`
+	DOB string                      `bson:"dob" json:"dob" binding:"required"`
 }
