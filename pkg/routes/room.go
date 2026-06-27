c3005b9c9c3e7519d076b0d53a2b6d5ee8194a5e
sinhaaman055 <sinhaaman055@gmail.com>
2026-06-28 00:36:14 +0530

    Adding new routes for LEAVE And KICK

--- pkg/routes/room.go
+++ pkg/routes/room.go
@@ -9,4 +9,6 @@ import (
 func RoomRoutes(r *gin.Engine){
 	r.POST("/createRoom",middleware.Authenticate(),handlers.CreateRoom)
 	r.POST("/joinRoom",middleware.Authenticate(),handlers.JoinRoom)
+	r.POST("/kick/:id",middleware.Authenticate(),handlers.Kick)
+	r.POST("/leave/:id",middleware.Authenticate(),handlers.Leave)
 }
