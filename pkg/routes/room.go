4bc90ae59fb7086d3106e1aeefb5990b1bbbcc5b
sinhaaman055 <sinhaaman055@gmail.com>
2026-07-14 02:27:58 +0530

    Adding routes  for live chat/websockets

--- pkg/routes/room.go
+++ pkg/routes/room.go
@@ -11,4 +11,5 @@ func RoomRoutes(r *gin.Engine){
 	r.POST("/joinRoom",middleware.Authenticate(),handlers.JoinRoom)
 	r.POST("/kick/:id",middleware.Authenticate(),handlers.Kick)
 	r.POST("/leave/:id",middleware.Authenticate(),handlers.Leave)
+	r.GET("/ws/:roomId",middleware.Authenticate(),handlers.Wshandler)
 }
